4060c9e1a58eca2fc9dd9a6462603a8986f0379d
Matthiew Purple <matthiewpurple@hotmail.com>
2023-05-12 11:56:53 +0200

    i

--- NormalInstakillResistances.cs
+++ NormalInstakillResistances.cs
@@ -3,7 +3,7 @@ using Il2Cpp;
 using normal_instakill_resistances;
 using Il2Cppresult2_H;
 
-[assembly: MelonInfo(typeof(NormalInstakillResistances), "Normal instakill resistances", "1.0.0", "Matthiew Purple")]
+[assembly: MelonInfo(typeof(NormalInstakillResistances), "Normal Instakill resistances", "1.0.0", "Matthiew Purple")]
 [assembly: MelonGame("アトラス", "smt3hd")]
 
 namespace normal_instakill_resistances;
